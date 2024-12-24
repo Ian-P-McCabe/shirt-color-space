0fd880d4ea29f4f239cc2fbd9c7e7244d777746c
Ian-P-McCabe <ianpmccabe@gmail.com>
2024-12-24 17:26:18 -0500

    HTML writeup in progress

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ Too many teams in the dodgeball league are the same color.
 This project tires to solve this issue with math and the 
 Lab colorspace.
 
-<iframe src="Chosen_Colors_test.html" width="750px" height="750px" id="testiframe"></iframe>
-
 # Useful Links
 
 - Pros and Cons of Lab color: https://graphicdesign.stackexchange.com/questions/76824/what-are-the-pros-and-cons-of-using-lab-color
--- script.js
+++ script.js
@@ -35,9 +35,9 @@ colorList.forEach(color => {
     const label = document.createElement('div');
     label.textContent = `${color["Shirt Color"]}`;
     label.style.marginTop = '5px';
-    label.style.fontSize = '14px';
+    label.style.fontSize = '16px';
     label.style.color = '#333'; // Adjust the color as needed
-    label.style.maxWidth = '75px'
+    label.style.maxWidth = '100px'
 
     // Append the rectangle and label to the container
     itemContainer.appendChild(rect);
